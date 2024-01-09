10cfed964a4c11b38b080fbd63fe9a37eba34503
OwaisaliBaig <obaig@evolvedtax.com>
2024-01-09 11:01:15 -0500

    1098 Add

---
+++ EvolvedTax_Institute/Areas/1098/Controllers/Form1098Controller.cs
@@ -0,0 +1,15 @@
+﻿using EvolvedTax.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EvoTax._1098.Controllers
+{
+    [Area("1098")]
+    public class Form1098Controller : BaseController
+    {
+        public IActionResult SelectForm(int entityId)
+        {
+            HttpContext.Session.SetInt32("EntityId", entityId);
+            return View();
+        }
+    }
+}
---
+++ EvolvedTax_Institute/Areas/1098/Controllers/Form1098_Q_Controller.cs
@@ -0,0 +1,143 @@
+﻿using EvolvedTax.Business.Services.AnnouncementService;
+using EvolvedTax.Business.Services.CommonService;
+using EvolvedTax.Business.Services.Form1042Services;
+using EvolvedTax.Business.Services.Form1098Services;
+using EvolvedTax.Business.Services.Form1099Services;
+using EvolvedTax.Business.Services.Form5498ervices;
+using EvolvedTax.Business.Services.InstituteService;
+using EvolvedTax.Common.Constants;
+using EvolvedTax.Data.Models.DTOs.Request;
+using EvolvedTax.Helpers;
+using EvolvedTax.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EvolvedTax_Institute.Areas._1098.Controllers
+{
+    [Area("1098")]
+    public class Form1098_Q_Controller : BaseController
+    {
+        private readonly IForm1098_Q_Service _form1098_Q_Service;
+        private readonly IInstituteService _instituteService;
+        private readonly ICommonService _commonService;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public Form1098_Q_Controller(IForm1098_Q_Service form1098_Q_Service, IWebHostEnvironment webHostEnvironment, ICommonService commonService, IInstituteService instituteService)
+        {
+            _form1098_Q_Service = form1098_Q_Service;
+            _webHostEnvironment = webHostEnvironment;
+            _commonService = commonService;
+            _instituteService = instituteService;
+        }
+        public IActionResult Index()
+        {
+            var EntityId = HttpContext.Session.GetInt32("EntityId") ?? 0;
+            ViewBag.EntitiesList = _instituteService.GetEntitiesByInstId(SessionUser.InstituteId, Convert.ToInt32(AppConstants.FormSubscription_1098)).Select(p => new SelectListItem
+            {
+                Text = p.EntityName,
+                Value = p.EntityId.ToString(),
+                Selected = p.EntityId == EntityId
+            });
+            return View(_form1098_Q_Service.GetForm1098QList().Where(p => p.EntityId == EntityId));
+        }
+        [Route("Form1098_Q/uploadClients")]
+        [HttpPost]
+        public async Task<IActionResult> UploadClients(IFormFile file, int EntityId, string entityName)
+        {
+            if (file == null)
+            {
+                return Json(false);
+            }
+            var response = await _form1098_Q_Service.Upload1098_Q_Data(file, SessionUser.InstituteId, EntityId, SessionUser.UserId);
+            return Json(response);
+        }
+        public IActionResult ChangeEntity(int entityId)
+        {
+            //int InstId = HttpContext.Session.GetInt32("InstId") ?? 0;
+            HttpContext.Session.SetInt32("EntityId", entityId);
+            var response = _form1098_Q_Service.GetForm1098QList().Where(p => p.EntityId == entityId);
+            //return RedirectToAction("Index");
+            return Json(new { Data = "true" });
+        }
+        #region PDF Creation Methods
+        [HttpGet]
+        public IActionResult DownlodPdf(int Id)
+        {
+            string TemplatePathFile = Path.Combine(_webHostEnvironment.WebRootPath, "Forms", AppConstants.Form1042STemplateFileName);
+            string SavePathFolder = Path.Combine(_webHostEnvironment.WebRootPath, AppConstants.Form1042S);
+            var entityId = HttpContext.Session.GetInt32("EntityId") ?? 0;
+            string pdfUrl = _form1098_Q_Service.GeneratePdf(Id, TemplatePathFile, SavePathFolder, entityId);
+            return Json(pdfUrl);
+        }
+
+        [HttpPost]
+
+        [Route("Form1098_Q/DownloadAll")]
+        public IActionResult DownloadAll([FromBody] DownloadRequestModel model)
+        {
+
+            List<int> ids = model.ids;
+            List<string> selectedPage = model.selectedPage;
+            string RootPath = _webHostEnvironment.WebRootPath;
+            string SavePathFolder = Path.Combine(_webHostEnvironment.WebRootPath, AppConstants.Form1042S);
+            var zipFilePath = _form1098_Q_Service.GenerateAndZipPdfs(ids, SavePathFolder, selectedPage, RootPath);
+            string contentType = "application/zip";
+
+            var fileBytes = System.IO.File.ReadAllBytes(zipFilePath);
+            return File(fileBytes, contentType, "GeneratedPDFs.zip");
+
+
+        }
+
+        [HttpPost]
+        [Route("Form1098_Q/DownloadOneFile")]
+        public IActionResult DownloadOneFile([FromBody] DownloadRequestModel model)
+        {
+
+            List<int> ids = model.ids;
+            List<string> selectedPage = model.selectedPage;
+            string RootPath = _webHostEnvironment.WebRootPath;
+            string SavePathFolder = Path.Combine(_webHostEnvironment.WebRootPath, AppConstants.Form1042S);
+            //   bool containsAll = selectedPage.Contains("All");
+
+            //    if (containsAll)
+            //    {
+            //    selectedPage.Clear();
+            //    selectedPage.Add("2");
+            //    selectedPage.Add("3");
+            //    selectedPage.Add("4");
+            //    selectedPage.Add("6");
+            //    selectedPage.Add("7");
+            //}
+            var zipFilePath = _form1098_Q_Service.DownloadOneFile(ids, SavePathFolder, selectedPage, RootPath);
+            string contentType = "application/zip";
+
+            var fileBytes = System.IO.File.ReadAllBytes(zipFilePath);
+            return File(fileBytes, contentType, "GeneratedPDFs.zip");
+
+
+        }
+        #endregion
+
+
+        [HttpPost]
+        public async Task<IActionResult> KeepRecord(int id)
+        {
+            if (id == 0)
+            {
+                return Json(false);
+            }
+            var response = await _form1098_Q_Service.KeepRecord(id);
+            return Json(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteRecord(int id)
+        {
+            if (id == 0)
+            {
+                return Json(false);
+            }
+            var response = await _form1098_Q_Service.DeletePermeant(id);
+            return Json(response);
+        }
+    }
+}
