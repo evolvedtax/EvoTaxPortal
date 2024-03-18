05f20e2001d184b2066939167cfd1b70d1a8a5e8
OwaisaliBaig <obaig@evolvedtax.com>
2024-03-18 13:29:16 -0400

    Changes

--- EvolvedTax.Common/Constants/URLConstatnts.cs
+++ EvolvedTax.Common/Constants/URLConstatnts.cs
@@ -5,9 +5,16 @@ namespace EvolvedTax.Common.Constants
 {
     public static class URLConstants
     {
-        public const string InstituteUrl = @"www.admin.evolvedforms.com";
-        public const string EvoTaxPortalUrl = @"www.sysadmin.evolvedforms.com";
-        public const string ClientUrl = @"www.evolvedforms.com";
-        public const string RecipientUrl = @"admin.evolvedforms.com";
+        //public const string InstituteUrl = @"www.admin.evolvedforms.com";
+        //public const string EvoTaxPortalUrl = @"www.sysadmin.evolvedforms.com";
+        //public const string ClientUrl = @"www.evolvedforms.com";
+        //public const string RecipientUrl = @"admin.evolvedforms.com";
+
+
+
+        public const string InstituteUrl = @"www.adminw8w9.evolvedtoolkits.com";
+        public const string EvoTaxPortalUrl = @"www.sysadminw8w9.evolvedtoolkits.com";
+        public const string ClientUrl = @"www.w8w9.evolvedtoolkits.com";
+        public const string RecipientUrl = @"adminw8w9.evolvedtoolkits.com";
     }
 }
